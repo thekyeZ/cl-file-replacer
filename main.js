2648292e714a8de58e81c3555eb43a2c8db890ae
kyez <przemek.urbaniak@hotmail.com>
2019-10-23 11:45:12 +0200

    Working for codio directories

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 var fs = require('fs');
 // var path = require('path');
 
-fs.readFile('./gulpfile.js', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/browsers/g, 'overrideBrowserslist');
+const { readdirSync } = require('fs')
 
-  fs.writeFile('./test.js', result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-});
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .filter(dirent => dirent.name[0] != ".")
+    .map(dirent => dirent.name)
+    .forEach(el => replacecon(el));
+
+getDirectories('./')    
+
+function replacecon(file) {
+    const path = file +'/gulpfile.js'
+    fs.readFile(path, 'utf8', function (err,data) {
+        if (err && err.errno == -2) {
+            return console.log(`Nie znaleziono gulpfile.js w katalogu `+path);
+        } 
+
+        console.log(`Znaleziono gulpfile.js w katalogu `+path)
+        console.log(data);
+        var result = data.replace(/browsers/g, 'overrideBrowserslist');
+        
+        fs.writeFile(path, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+}
+
